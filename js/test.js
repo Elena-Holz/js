b0e2de962d2209bf0a61f6b06db455cc5189b88e
Elena-Holz <elenholz@gmail.com>
2022-05-06 00:19:53 +0300

    add-test

---
+++ js/test.js
@@ -0,0 +1,18 @@
+
+const firstName = 'Holovina';
+const lastName = 'Elena';
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+const street = "Zadova";
+const house = '1-G';
+const flat = '10';
+
+// const adress = `${fullName} ${house} ${flat}`;
+
+const adress = `Пани ${fullName} живет по адресу ${street} ${house} ${flat}`;
+console.log(adress);
+
+// const ticket = 10;
+// const order = `Вы заказали ${ticket} билетов`;
+// console.log(order);
